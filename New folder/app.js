bd4f8a5a57fb8535fcf2471c3bd3439d193df6ea
zain ali <mzain42401@gmail.com>
2022-12-26 22:56:54 +0500

    practice

---
+++ New folder/app.js
@@ -0,0 +1,18 @@
+let num=document.getElementById("num")
+
+let count=0
+
+const func=(x,y)=>{
+    num.innerHTML=x+y
+    console.log(count);
+}
+
+
+const func1=(x,y)=>{
+    num.innerHTML=x*0/y
+    console.log(count);
+}
+
+for (var i=0;i>0;i++){
+num.innerHTML=i
+}
